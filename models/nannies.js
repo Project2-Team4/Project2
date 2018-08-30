1d88cb0b845be21cf3c7524760726df4f1557296
Kris Wilson <krispykrisp12@gmail.com>
2018-08-30 19:10:55 -0400

    Updates

--- models/nannies.js
+++ models/nannies.js
@@ -1,40 +1,40 @@
 module.exports = function(sequelize, DataTypes) {
-  var Nanny = sequelize.define("Nanny", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    age: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    city: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    availability: {
-      type: DataTypes.BOOLEAN,
-      default: false
-    },
-    yearsOfExp: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    hourlyRate: {
-      type: DataTypes.INTEGER,
-    },
-    photo: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  });
-  return Nanny;
+  // var Nanny = sequelize.define("Nanny", {
+  //   name: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false
+  //   },
+  //   age: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false
+  //   },
+  //   city: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false
+  //   },
+  //   phone: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false
+  //   },
+  //   email: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false
+  //   },
+  //   availability: {
+  //     type: DataTypes.BOOLEAN,
+  //     default: false
+  //   },
+  //   yearsOfExp: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false
+  //   },
+  //   hourlyRate: {
+  //     type: DataTypes.INTEGER,
+  //   },
+  //   photo: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false
+  //   }
+  // });
+  // return Nanny;
 };
